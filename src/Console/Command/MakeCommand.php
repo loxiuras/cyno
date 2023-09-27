6a61f26a7e29ae32808381064333ed60ed631f32
Peter van Garderen <pvangarderen@outlook.com>
2023-09-27 23:02:21 +0200

    Updated

--- src/Console/Command/MakeCommand.php
+++ src/Console/Command/MakeCommand.php
@@ -124,11 +124,6 @@ class MakeCommand extends Command
 
     public function saveFile(): void
     {
-        var_dump($this->getFileDirectoryLocation(true));
-        var_dump(is_dir($this->getFileDirectoryLocation(true)));
-        var_dump(file_exists($this->getFileDirectoryLocation(true)));
-        die;
-
         if (!is_dir($this->getFileDirectoryLocation(true))) {
             mkdir($this->getFileDirectoryLocation(true));
         }
