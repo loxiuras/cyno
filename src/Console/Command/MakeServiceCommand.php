b189d773eeea5cea3b8340787873f9a96b0381c0
Peter van Garderen <pvangarderen@outlook.com>
2023-09-27 21:20:09 +0200

    Updated

--- src/Console/Command/MakeServiceCommand.php
+++ src/Console/Command/MakeServiceCommand.php
@@ -8,29 +8,19 @@ use JetBrains\PhpStorm\NoReturn;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'make:service')]
+#[AsCommand(
+    name: 'make:service',
+    description: 'Creates a new service file.',
+    hidden: false
+)]
 class MakeServiceCommand extends Command
 {
-    protected function configure(): void
-    {
-//        $this
-//            ->setDefinition(
-//                [
-//                    new InputArgument('make', InputArgument::OPTIONAL, ''),
-//                ]
-//            )
-//            ->setDescription('Make rule')
-//        ;
-    }
-
     #[NoReturn]
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        die('End...');
 
-        return 0;
+        return Command::SUCCESS;
     }
 }
